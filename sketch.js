a4fa8c298d7aa6c054f672adec30530359d77626
Matteo-Ligori <matteo.ligori@mail.polimi.it>
2022-01-28 17:07:22 +0100

    Revert "final"
    This reverts commit fc845376ef3fca9f830879ce4d6eb6f5600c2c60.

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
 let sfondo;
 let start;
 var input;
-//prendo link pagina
+
 const urlString = window.location.href;
 let url = new URL(urlString);
 
 function preload() {
-  //inserisco sfondo e titolo
   sfondo = loadImage("./assets/sfondo-01.png");
   titolo = loadImage("./assets/titolo.png");
 }
@@ -15,10 +14,9 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
   background("powderblue");
-  //creo bottone html, stile modificato tramuite il file html
   start = createButton("Adopt!");
   start.mouseClicked(goTo);
-  //inserisco elemento html input modificato nel file html
+
   input = createInput("").attribute("placeholder", "Type here");
 }
 
@@ -36,20 +34,17 @@ function draw() {
   textFont("Titan One");
   textSize(35);
 
-  //quando scrivo nell'input la stringa viene riscritta sotto con un punto esclamativo
   var myText = input.value();
   if (input.value() == "") {
     text("", width / 2, height / 2 + 50);
   } else {
     text(myText + "!", width / 2 - 10, (height * 2) / 3);
   }
-  // utilizzo storeItem, funzione prefatta che permette di registrare nella sessione attuale un elemento, in questo caso la stringa del nome
-  // in questo modo quando si andrà nell'altra pagina html, verrà trascritto anche se su due pagine html separate
+
   storeItem("myText", myText);
 }
 
 function goTo() {
-  //richiamo la pagine che verrà aperta in seguito
   if (input.value() != "") {
     window.open(url + "second-page.html?" + input.value(), "_self");
   }
